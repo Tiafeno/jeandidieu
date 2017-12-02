3586d3bf9ad5d49215483ee12b8d17b6b6ba7027
Tiafeno <tiafenofnel@gmail.com>
2017-12-02 18:49:15 +0300

    Add another paragraphs

--- assets/js/jeandidieu.js
+++ assets/js/jeandidieu.js
@@ -1,12 +1,21 @@
 (function ($) {
   $(document).ready(function () {
     var windowHeight = $(window).innerHeight();
+    var windowWidth = 0;
     var container = $("main#container, #scroll-container");
+    var group = $('.group-container');
     $("html").mousewheel(function (event, delta) {
       this.scrollLeft -= (delta * 30);
       event.preventDefault();
     });
 
+    /** set exactelly body width */
+    var paragraphs = group.find('.paragraphs-items');
+    paragraphs.each(function(index, element) {
+      var currentWidth = $( this ).width();
+      windowWidth += parseFloat(currentWidth);
+      posWindow(); 
+    })
     positionHeight()
       .then(function successCallback(height) {
         container.css({height: height + "px", visibility: 'visible'});
@@ -18,6 +27,10 @@
         container.css({height: height + "px", visibility: 'visible'});
       });
     });
+
+    function posWindow() {
+      group.width(windowWidth);
+    }
 
     function positionHeight() {
       return new Promise(function(resolve, reject) {
