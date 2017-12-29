e2586669a454b1ebded5b82b096dcc03034a5854
Tiafeno <tiafenofnel@gmail.com>
2017-12-29 23:30:49 +0300

    add mail validate

--- assets/js/jeandidieu.js
+++ assets/js/jeandidieu.js
@@ -12,6 +12,11 @@
 
     $("#formCommand").submit(function() {
       var mail = $('input[type=mail]').val().trim();
+      var regexMail =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (! regexMail.test(mail.toLowerCase())) {
+        alert( "Veillez remplir le champ avec votre adresse email." );
+        return;
+      }
       $.get("controller.php", {
         method: "sendMail",
         sender: window.btoa(mail)
