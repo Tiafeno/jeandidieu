222ecdb7e8419e529e3dea312dfd879070e434cb
Tiafeno <tiafenofnel@gmail.com>
2018-01-05 11:46:29 +0300

    Fix return on click submit form

--- assets/js/jeandidieu.js
+++ assets/js/jeandidieu.js
@@ -15,7 +15,7 @@
       var regexMail =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if (! regexMail.test(mail.toLowerCase())) {
         alert( "Veillez remplir le champ avec votre adresse email." );
-        return;
+        return false;
       }
       $.get("controller.php", {
         method: "sendMail",
